07bade555852bd6c735019f50a997775f7ca7ff7
Jimleerx <41221019+Jimleerx@users.noreply.github.com>
2023-05-31 16:02:11 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -2,7 +2,12 @@
 wget -O /app/public/locales/zh-CN/common.json https://cdn.jsdelivr.net/gh/Jimleerx/homepage-qbittorrent-size@master/common.json
 wget -O /app/src/widgets/qbittorrent/component.jsx https://cdn.jsdelivr.net/gh/Jimleerx/homepage-qbittorrent-size@master/component.jsx
 wget -O /app/.next.zip https://cdn.jsdelivr.net/gh/Jimleerx/homepage-qbittorrent-size@master/next.zip
-mv /app/.next /app/.next.old
+if [ ! -d "/app/.next.old" ]; then
+  mv /app/.next /app/.next.old
+else 
+  rm -rf /app/.next
+fi
+
 unzip -d /app /app/.next.zip 
 chown -R node:node /app/.next
 chmod 644 `find /app/.next -type f`
